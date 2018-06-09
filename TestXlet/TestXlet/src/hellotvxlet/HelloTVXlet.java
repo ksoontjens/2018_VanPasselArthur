c4e936591ebc09b3ea42ba8c0fa7db26e87e364e
arthurvanpassel <arthur.vanpassel@student.kdg.be>
2018-06-09 20:36:40 +0200

    toevoegen knoppen en kleuren

--- TestXlet/TestXlet/src/hellotvxlet/HelloTVXlet.java
+++ TestXlet/TestXlet/src/hellotvxlet/HelloTVXlet.java
@@ -15,15 +15,158 @@ public class HelloTVXlet implements Xlet, HActionListener {
     
     // Variabele om de actuele Xlet?context in te bewaren
     private XletContext actueleXletContext ;
-    
     private HScene scene;
-    
     private HSceneTemplate sceneTemplate = new HSceneTemplate();
     
+    // Componenten
     private HStaticText tekstLabel;
-    
-    private HTextButton knop1, knop2;
+    private HTextButton buttons[] = new HTextButton[16];
    
+    // Variabelen 
+    private int cards[] = new int[16];
+    private boolean firstTurn = true;
+    private int firstCard;
+    private int secondCard;
+    private boolean chosenCards[] = new boolean[16];
+    
+    public void setupKnoppen() {
+        // for lus om knoppen in te stellen
+        
+        int kolom = 0;
+        for(int i = 0;i<16;i++) {
+            buttons[i] = new HTextButton("");                 
+            buttons[i].setBackground(Color.GRAY);
+            buttons[i].setBackgroundMode(HVisible.BACKGROUND_FILL);
+            buttons[i].setSize(100,100); 
+            int rij = i % 4;
+            buttons[i].setLocation(20 + kolom*110,110 + rij*110);
+            if (rij==3) {
+                kolom++;
+            }
+        }
+
+        // pijltjes beweging toevoegen
+        buttons[0].setFocusTraversal(null, buttons[1], null, buttons[4]); // op, neer, links, rechts
+        buttons[1].setFocusTraversal(buttons[0], buttons[2], null, buttons[5]);
+        buttons[2].setFocusTraversal(buttons[1], buttons[3], null, buttons[6]);
+        buttons[3].setFocusTraversal(buttons[2], null, null, buttons[7]);
+        buttons[4].setFocusTraversal(null, buttons[5], buttons[0], buttons[8]);
+        buttons[5].setFocusTraversal(buttons[4], buttons[6], buttons[1], buttons[9]);
+        buttons[6].setFocusTraversal(buttons[5], buttons[7], buttons[2], buttons[10]);
+        buttons[7].setFocusTraversal(buttons[6], null, buttons[3], buttons[11]);
+        buttons[8].setFocusTraversal(null, buttons[9], buttons[4], buttons[12]);
+        buttons[9].setFocusTraversal(buttons[8], buttons[10], buttons[5], buttons[13]);
+        buttons[10].setFocusTraversal(buttons[9], buttons[11], buttons[6], buttons[14]);
+        buttons[11].setFocusTraversal(buttons[10], null, buttons[7], buttons[15]);
+        buttons[12].setFocusTraversal(null, buttons[13], buttons[8], null);
+        buttons[13].setFocusTraversal(buttons[12], buttons[14], buttons[9], null);
+        buttons[14].setFocusTraversal(buttons[13], buttons[15], buttons[10], null);
+        buttons[15].setFocusTraversal(buttons[14], null, buttons[11], null);
+        
+        // knoppen aan de Scene toevoegen
+        for (int i = 0;i<16;i++) {
+            scene.add(buttons[i]);
+        }
+        
+        // Startpunt selectie
+        buttons[0].requestFocus();
+        
+        // acties koppelen aan knoppen
+        for (int i = 0;i<16;i++) {
+                buttons[i].setActionCommand(""+i);
+                buttons[i].addHActionListener(this);
+        }
+        
+        System.out.println("Knoppen ingesteld");
+    }
+
+    public void setupCards() {
+        // Maak een randomgenerator aan
+        Random rand = new Random();
+        //System.out.println("test");
+        int cardsOpVolgorde[] = {1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8};
+        boolean cardAssigned = false;
+        int i = 0;
+        for(i=0;i<16;i++) {
+            chosenCards[i] = false;
+            cardAssigned = false;
+            do {
+                //System.out.println("TEST");
+                int n = rand.nextInt(16);    // van 0 - 15
+                if (cardsOpVolgorde[n] != 0) {
+                    cards[i] = cardsOpVolgorde[n];
+                    cardsOpVolgorde[n] = 0;
+                    cardAssigned = true;
+                }
+            } while (!cardAssigned);
+            System.out.println("Kaart "+i+": "+cards[i]);
+        }
+    }
+    
+    public void actionPerformed (ActionEvent e){
+            System.out.println("knop" + e.getActionCommand() + "_actioned");
+            int i = Integer.parseInt(e.getActionCommand());
+
+            giveColor(i);
+            checkCard(i);
+    }
+    
+    public void giveColor(int i) {
+        if (cards[i] == 1) {
+            buttons[i].setBackground(Color.BLUE);
+        }
+        
+        if (cards[i] == 2) {
+            buttons[i].setBackground(Color.RED);
+        }
+        
+        if (cards[i] == 3) {
+            buttons[i].setBackground(Color.YELLOW);
+        }
+        
+        if (cards[i] == 4) {
+            buttons[i].setBackground(Color.ORANGE);
+        }
+        
+        if (cards[i] == 5) {
+            buttons[i].setBackground(Color.GREEN);
+        }
+        
+        if (cards[i] == 6) {
+            buttons[i].setBackground(Color.WHITE);
+        }
+        
+        if (cards[i] == 7) {
+            buttons[i].setBackground(Color.PINK);
+        }
+        if (cards[i] == 8) {
+            buttons[i].setBackground(Color.BLACK);
+        }
+        chosenCards[i] = true;
+    }
+    
+    public void checkCard(int i) {
+        if (firstTurn) {
+            buttons[secondCard].setBackground(Color.GRAY);
+            buttons[firstCard].setBackground(Color.GRAY);
+            firstTurn = false;
+            firstCard = i;
+        }
+        else {
+            secondCard = i;
+            if (cards[secondCard] == cards[firstCard]) {
+                System.out.println("JUIST GERADEN!");
+                chosenCards[secondCard] = true;
+            }
+            else {
+                System.out.println("FOUT GERADEN!");
+                chosenCards[firstCard] = false;
+                chosenCards[secondCard] = false;
+            }
+            firstTurn = true;
+        }
+    }
+    
     // Initialiseren van de benodigde resources en variabelen :
     public void initXlet ( XletContext context ) {
         this.actueleXletContext = context ;
@@ -42,46 +185,19 @@ public class HelloTVXlet implements Xlet, HActionListener {
         scene = HSceneFactory.getInstance().getBestScene(sceneTemplate);
         
         // object aanmaken
-        tekstLabel = new HStaticText("TEKST");
-        
+        tekstLabel = new HStaticText("--- MEMORY ---");
         // eigenschappen van tekstLabel instellen
-        tekstLabel.setLocation(100,100);
-        tekstLabel.setSize(400,250);
-        tekstLabel.setBackground(new DVBColor(255,255,255,179));
+        tekstLabel.setLocation(20,20);
+        tekstLabel.setSize(680,80);
+        tekstLabel.setBackground(Color.BLUE);
         tekstLabel.setBackgroundMode(HVisible.BACKGROUND_FILL);
         
         // tekstLabel aan de Scene toevoegen
         scene.add(tekstLabel);
         
-        // eigenschappen van knoppen instellen
-        knop1 = new HTextButton("KNOP1");
-        knop1.setLocation(100,400);
-        knop1.setSize(100,50);
-        knop1.setBackground(new DVBColor(0,0,0,179));
-        knop1.setBackgroundMode(HVisible.BACKGROUND_FILL);
-        
-        knop2 = new HTextButton("KNOP2");
-        knop2.setLocation(300,400);
-        knop2.setSize(100,50);
-        knop2.setBackground(new DVBColor(0,0,0,179));
-        knop2.setBackgroundMode(HVisible.BACKGROUND_FILL);
-        
-        // pijltjes beweging toevoegen
-        knop1.setFocusTraversal(null, null, null, knop2); // op, neer, links, rechts
-        knop2.setFocusTraversal(null, null, knop1, null);
-        
-        // knoppen aan de Scene toevoegen
-        scene.add(knop1);
-        scene.add(knop2);
-        
-        // Startpunt selectie
-        knop1.requestFocus();
-        
-        // acties koppelen aan knoppen
-        knop1.setActionCommand("knop1_actioned");
-        knop2.setActionCommand("knop2_actioned");
-        knop1.addHActionListener(this);
-        knop2.addHActionListener(this);
+        // Knoppen installen
+        setupKnoppen();
+        setupCards();
     }
     
     // Starten van de Xlet :
@@ -92,6 +208,7 @@ public class HelloTVXlet implements Xlet, HActionListener {
         // Scene zichtbaar maken
         scene.validate();
         scene.setVisible(true);
+        
     }
     
     // Methode voor de pause toestand .
@@ -109,9 +226,5 @@ public class HelloTVXlet implements Xlet, HActionListener {
             System.out.println( "De mogelijkheid bestaat "+"door het werpen van een exceptie "+"de Xlet in leven te houden. " ) ;
             throw new XletStateChangeException ( " Laat me leven ! " ) ;
         }
-    }
-
-    public void actionPerformed (ActionEvent e){
-        System.out.println(e.getActionCommand());
     }
 }
